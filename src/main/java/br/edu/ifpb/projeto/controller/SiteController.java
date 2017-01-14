033c757834256aa91a35771db270da0a6332cccb
Joffily Ferreira <joffily@outlook.com>
2017-01-14 14:43:14 -0300

    Atualizado o SiteController com o novo método de chamada

--- src/main/java/br/edu/ifpb/projeto/controller/SiteController.java
+++ src/main/java/br/edu/ifpb/projeto/controller/SiteController.java
@@ -4,9 +4,13 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class SiteController {
+public class SiteController extends ApplicationController {
 
-	public RequestDispatcher index(HttpServletRequest request, HttpServletResponse response) {
+	public SiteController(HttpServletRequest request, HttpServletResponse response) {
+		super(request, response);
+	}
+
+	public RequestDispatcher index() {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/view/site/index.jsp");
 		return dispatcher;
 	}	
