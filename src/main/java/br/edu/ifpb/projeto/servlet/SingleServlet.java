57219e9a87a3c0ba71f2742e6be6f041147e536f
Joffily F <joffily@outlook.com>
2017-01-17 14:12:26 -0300

    Adicionando Rotas
    Este commit introduz o conceito de rotas personalizadas ao Projeto
    A Classe Router é responsável por guardar todas as rotas do sistema.
    
    Para adicionar uma nova rota devemos editar o construtor da classe Router
    da seguinte maneira:
    
    addRoute("/usuario/cadastro", "UsuarioController", "cadastro")
    Onde o primeiro parâmetro é a URL, o segundo é o Controller e o terceiro é o método do Controller

--- src/main/java/br/edu/ifpb/projeto/servlet/SingleServlet.java
+++ src/main/java/br/edu/ifpb/projeto/servlet/SingleServlet.java
@@ -2,6 +2,7 @@ package br.edu.ifpb.projeto.servlet;
 
 import java.io.IOException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,45 +11,50 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/")
+@WebServlet(urlPatterns = { "", "/" })
 public class SingleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String[] path = request.getRequestURI().split("/");
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String controllerPackage = "br.edu.ifpb.projeto.controller.";
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/view/site/index.jsp");
 
-		if (path.length > 2 && path[2] != null) {
+		Router router = new Router();
+		String requestedURL = request.getServletPath();
 
-			String controllerName = path[2] + "Controller";
-			String methodName = "index";
-			
-			controllerName = controllerName.substring(0, 1).toUpperCase() + controllerName.substring(1);
-			
-			if (path.length >= 4 && path[3] != null) {
-				methodName = path[3];
-			}
-
-			try {
-				Class<?> controller = Class.forName(controllerPackage + controllerName);
-
-				// Create a new classed with request and response objects
-				Object obj = controller.getDeclaredConstructor(HttpServletRequest.class, HttpServletResponse.class).newInstance(request, response);
-
-				// Call the method
-				Method method = controller.getMethod(methodName);
-
-				// dispatch with that result of method invoked previously 
-				dispatcher = (RequestDispatcher) method.invoke(obj);
-			} catch (Exception e) {
-				dispatcher = request.getRequestDispatcher("/view/helper/404.jsp");
-				e.printStackTrace();
-			}
+		if (requestedURL.equals("")) {
+			requestedURL = "/";
 		}
-		
+
+		ArrayList<String> rcontroller = router.getURL(requestedURL);
+
+		if (rcontroller == null) {
+			dispatcher = request.getRequestDispatcher("/view/helper/404.jsp");
+			return;
+		}
+
+		try {
+			Class<?> controller = Class.forName(controllerPackage + rcontroller.get(0));
+
+			// Create a new classed with request and response objects
+			Object obj = controller.getDeclaredConstructor(HttpServletRequest.class, HttpServletResponse.class)
+					.newInstance(request, response);
+
+			// Call the method
+			Method method = controller.getMethod(rcontroller.get(1));
+
+			// dispatch with that result of method invoked previously
+			dispatcher = (RequestDispatcher) method.invoke(obj);
+		} catch (Exception e) {
+			dispatcher = request.getRequestDispatcher("/view/helper/404.jsp");
+			e.printStackTrace();
+		}
+
 		if (!response.isCommitted()) {
-			dispatcher.forward(request, response);	
+			dispatcher.forward(request, response);
 		}
+
 	}
 }
