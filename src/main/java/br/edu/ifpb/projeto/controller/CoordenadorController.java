a923b518081e70ba3a9ac5545c15341e89de9ebf
André <andrealencar@outlook.com.br>
2017-01-26 22:49:41 -0300

    Criação do Painel de administração (Coordenador)
    Validação do tipo de ID da empresa e remoção do trecho else

--- src/main/java/br/edu/ifpb/projeto/controller/CoordenadorController.java
+++ src/main/java/br/edu/ifpb/projeto/controller/CoordenadorController.java
@@ -1,6 +1,7 @@
 package br.edu.ifpb.projeto.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
@@ -13,9 +14,8 @@ import br.edu.ifpb.projeto.model.Empresa;
 import br.edu.ifpb.projeto.model.Usuario;
 
 public class CoordenadorController extends ApplicationController{
-	
-	private EmpresaDAO empresaDAO = new EmpresaDAO(PersistenceUtil.getCurrentEntityManager());	
-	
+	private EmpresaDAO empresaDAO = new EmpresaDAO(PersistenceUtil.getCurrentEntityManager());
+		
 	public CoordenadorController(HttpServletRequest request, HttpServletResponse response) {
 		super(request, response);
 	}
@@ -34,19 +34,26 @@ public class CoordenadorController extends ApplicationController{
 	
 	public RequestDispatcher habilitarempresa() throws IOException {
 		RequestDispatcher dispatcher = this.request.getRequestDispatcher("/coordenacao");
-		int idNumber = Integer.parseInt(request.getParameter("id"));
 		HttpSession session = request.getSession();
-		
+		int idNumber; 
+
 		if (session.getAttribute("usuario") == null  || ((Usuario) session.getAttribute("usuario")).isCoordenador() == false) {
 			response.sendRedirect(request.getServletContext().getContextPath());
-		}else{
+		}
+		
+		if(request.getParameter("id").matches("\\d+")){
+			idNumber = Integer.parseInt(request.getParameter("id"));
+
 			Empresa empresa = empresaDAO.find(idNumber);
 			empresa.setHabilitada(true);
+
 			empresaDAO.beginTransaction();
 			empresaDAO.update(empresa);
 			empresaDAO.commit();
+		}else{
+			super.addFlashMessage("error", "Empresa não foi encontrada");
 		}
-		
+			
 		return dispatcher;
 	}
 }
