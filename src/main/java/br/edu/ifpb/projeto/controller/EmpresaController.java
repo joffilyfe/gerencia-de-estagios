60992ef0107ffe991cbb6ae7a576085b766c638c
Joffily Ferreira <joffily@outlook.com>
2017-02-04 17:38:11 -0300

    Removendo código duplicado (Habilitar empresa)
    Os dois arquivos do commit tinham código duplicado e que não eram utilizados para nada.

--- src/main/java/br/edu/ifpb/projeto/controller/EmpresaController.java
+++ src/main/java/br/edu/ifpb/projeto/controller/EmpresaController.java
@@ -12,14 +12,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import br.edu.ifpb.projeto.dao.AlunoDAO;
 import br.edu.ifpb.projeto.dao.EmpresaDAO;
-import br.edu.ifpb.projeto.dao.EstagioDAO;
 import br.edu.ifpb.projeto.dao.PersistenceUtil;
 import br.edu.ifpb.projeto.dao.VagaDAO;
-import br.edu.ifpb.projeto.model.Aluno;
 import br.edu.ifpb.projeto.model.Empresa;
-import br.edu.ifpb.projeto.model.Estagio;
 import br.edu.ifpb.projeto.model.Usuario;
 import br.edu.ifpb.projeto.model.Vaga;
 
@@ -28,8 +24,6 @@ public class EmpresaController extends ApplicationController {
 	// DAOS
 	private EmpresaDAO empresaDAO = new EmpresaDAO(PersistenceUtil.getCurrentEntityManager());
 	private VagaDAO vagaDAO = new VagaDAO(PersistenceUtil.getCurrentEntityManager());
-	private AlunoDAO alunoDAO = new AlunoDAO();
-	private EstagioDAO estagioDAO = new EstagioDAO();
 
 	public EmpresaController(HttpServletRequest request, HttpServletResponse response) {
 		super(request, response);
@@ -143,54 +137,6 @@ public class EmpresaController extends ApplicationController {
 			request.setAttribute("telefone", empresa.getTelefone());
 			request.setAttribute("fax", empresa.getFax());
 			request.setAttribute("email", empresa.getEmail());
-		}
-
-		return dispatcher;
-	}
-
-	/*
-	 * Método responsável por admitir candidatos em uma vaga específica
-	 */
-	public RequestDispatcher admitirCandidato() throws IOException {
-		RequestDispatcher dispatcher = this.request.getRequestDispatcher("/view/coordenador/listaCandidatos.jsp");
-		HttpSession session = request.getSession();
-
-		if (session.getAttribute("usuario") == null
-				|| ((Usuario) session.getAttribute("usuario")).isCoordenador() == false) {
-			response.sendRedirect(request.getServletContext().getContextPath());
-			return dispatcher;
-		}
-
-		int idAluno, idVaga, idEmpresa;
-
-		if (request.getMethod().equals("POST")) {
-			if (request.getParameter("idaluno").matches("^\\d+$")) {
-				idAluno = Integer.parseInt(request.getParameter("idaluno"));
-				Aluno aluno = alunoDAO.find(idAluno);
-				idVaga = Integer.parseInt(request.getParameter("idvaga"));
-				Vaga vaga = vagaDAO.find(idVaga);
-				idEmpresa = vaga.getEmpresa().getId();
-				Empresa empresa = empresaDAO.find(idEmpresa);
-				Estagio estagio = new Estagio(empresa, aluno, vaga);
-				this.request.setAttribute("estagio", estagio);
-
-				estagioDAO.beginTransaction();
-				estagioDAO.insert(estagio);
-				estagioDAO.commit();
-
-				aluno.getVagas().remove(vaga);
-
-				alunoDAO.beginTransaction();
-				alunoDAO.update(aluno);
-				alunoDAO.commit();
-
-				vaga.getAlunos().remove(aluno);
-
-				vagaDAO.beginTransaction();
-				vagaDAO.update(vaga);
-				vagaDAO.commit();
-
-			}
 		}
 
 		return dispatcher;
