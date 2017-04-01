dab260552f0a4fa6015cd8fbcd210474883eb871
Marianna Veríssimo <marianna1204@gmail.com>
2017-03-31 21:35:54 -0300

    Adicionada mensagem de informação para o usuário na realização do cadastro

--- src/main/java/br/edu/ifpb/projeto/bean/EmpresaBean.java
+++ src/main/java/br/edu/ifpb/projeto/bean/EmpresaBean.java
@@ -1,37 +1,32 @@
 package br.edu.ifpb.projeto.bean;
 
 import java.util.ArrayList;
-
 import java.util.List;
 import java.util.Map;
 
-import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
-
 import javax.faces.context.Flash;
 import javax.faces.event.ComponentSystemEvent;
 
-
+import br.edu.ifpb.project.util.Application;
 import br.edu.ifpb.projeto.dao.EmpresaDAO;
 import br.edu.ifpb.projeto.dao.VagaDAO;
 import br.edu.ifpb.projeto.model.Empresa;
 import br.edu.ifpb.projeto.model.Vaga;
 
-@ManagedBean(name="empresaBean")
+@ManagedBean(name = "empresaBean")
 @ViewScoped
 public class EmpresaBean {
 
 	private Empresa empresa = new Empresa();
 	private Vaga vaga = new Vaga();
-	private List<Vaga> vagas  = new ArrayList<Vaga>();
+	private List<Vaga> vagas = new ArrayList<Vaga>();
 	private List<Empresa> empresas = new ArrayList<Empresa>();
 	private int id;
-	
-	
+
 	public List<Empresa> getEmpresas() {
 		return empresas;
 	}
@@ -39,7 +34,7 @@ public class EmpresaBean {
 	public void setEmpresas(List<Empresa> empresas) {
 		this.empresas = empresas;
 	}
-	
+
 	public int getId() {
 		return id;
 	}
@@ -56,7 +51,6 @@ public class EmpresaBean {
 		this.vagas = vagas;
 	}
 
-
 	public Empresa getEmpresa() {
 		return empresa;
 	}
@@ -72,25 +66,22 @@ public class EmpresaBean {
 	public void setVaga(Vaga vaga) {
 		this.vaga = vaga;
 	}
-	
-	public String cadastrar()
-	{
+
+	public String cadastrar() {
 		vaga.setEmpresa(empresa);
-		
+
 		VagaDAO tDao = new VagaDAO();
 		tDao.beginTransaction();
 		tDao.insert(vaga);
 		tDao.commit();
 
-		
+		Application.addMessage("Vaga cadastrada com sucesso!", FacesMessage.SEVERITY_INFO);
 		vagas.add(vaga);
 		return "/view/empresa/minhas_vagas?faces-redirect=true";
 
-		
 	}
-	
-	public String editar()
-	{
+
+	public String editar() {
 		EmpresaDAO tDao = new EmpresaDAO();
 		tDao.beginTransaction();
 		tDao.update(empresa);
@@ -98,27 +89,27 @@ public class EmpresaBean {
 
 		return "/view/index?faces-redirect=true";
 	}
-	
+
 	public void listarVagas(ComponentSystemEvent event) {
-		
+
 		EmpresaDAO dao = new EmpresaDAO();
-	    
-	    empresa = dao.getById(empresa.getId());
-	    this.vagas = empresa.getVagas();
+
+		empresa = dao.getById(empresa.getId());
+		this.vagas = empresa.getVagas();
 	}
-	
+
 	public void listarEmpresas(ComponentSystemEvent event) {
-		
+
 		EmpresaDAO dao = new EmpresaDAO();
-	    
-	    this.empresas = dao.findAll();
-	   
+
+		this.empresas = dao.findAll();
+
 	}
-	
-	public String selecionar(Empresa empresa){
+
+	public String selecionar(Empresa empresa) {
 		this.empresa = empresa;
 		loadFlash();
-		return "/view/empresa/perfil_publico?faces-redirect=true&id="+empresa.getId();
+		return "/view/empresa/perfil_publico?faces-redirect=true&id=" + empresa.getId();
 	}
 
 	public String candidatos(Vaga vaga) {
@@ -126,34 +117,29 @@ public class EmpresaBean {
 		loadFlash();
 		return "/view/empresa/candidatos?faces-redirect=true";
 	}
-	
-	private void loadFlash(){
+
+	private void loadFlash() {
 		Flash flash = FacesContext.getCurrentInstance().getExternalContext().getFlash();
 		flash.put("empresa", empresa);
 		flash.put("vaga", vaga);
 	}
-	
-	public void unloadFlash(){
+
+	public void unloadFlash() {
 		Flash flash = FacesContext.getCurrentInstance().getExternalContext().getFlash();
 		this.setEmpresa((Empresa) flash.get(empresa));
 		this.setVaga((Vaga) flash.get("vaga"));
-		
+
 	}
 
-    public void init()
-	{
+	public void init() {
 		Map<String, Object> map = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
 		UsuarioBean user = (UsuarioBean) map.get("usuarioBean");
-		if(user.getUsuario() != null) {
+		if (user.getUsuario() != null) {
 			empresa = (Empresa) user.getUsuario();
-		}
-		else {
+		} else {
 			EmpresaDAO dao = new EmpresaDAO();
-		    empresa = dao.getById(id);
+			empresa = dao.getById(id);
 		}
 	}
-
-	
-	
 
 }
